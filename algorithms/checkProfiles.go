9549736053677f2340d88ca3f8cd23bfc32f6741
cnb1 <hackerrank134@gmail.com>
2022-08-17 15:25:45 -0400

    read profiles update

--- algorithms/checkProfiles.go
+++ algorithms/checkProfiles.go
@@ -12,14 +12,19 @@ func ReadProfiles() {
 
 	for !globals.QuitProgram {
 		time.Sleep(5 * time.Second)
-		fmt.Println("Checking if any users are ready to trade")
-
+		fmt.Println("")
+		fmt.Print("-> Users trading : ")
 		for e := globals.ProfilesToRun.Users.Front(); e != nil; e = e.Next() {
 			// go StartStopCommand()
 			userTemp := globals.User(e.Value.(globals.User))
 			if userTemp.Status == "nt" {
 				globals.UpdateStatus(userTemp)
 			}
+
+			if userTemp.Status == "t" {
+				fmt.Print(userTemp.Userid, " | ")
+			}
 		}
+		fmt.Println()
 	}
 }
